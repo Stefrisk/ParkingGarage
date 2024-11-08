8a7435a06a2d6cbfa4ff9f60390822d0c3bfa8a6
stefan frisk <stefrik@gmail.com>
2024-11-08 11:17:43 +0100

    generate MC brand method

--- ParkingGarage/Motorcycle.cs
+++ ParkingGarage/Motorcycle.cs
@@ -9,14 +9,47 @@ namespace ParkingGarage
 {
     internal class Motorcycle : Vehicle
     {
-        public string BrandName {  get; set; }
+        public string BrandName
+        {
+            get { return BrandName; }
+            set { BrandName = value; }
+        }
         public Motorcycle() : base()
         {
+
+            BrandName = Console.ReadLine();
             Size = 0.5;
             Color = GenColor();
             RegNum = GenRegNum();
-            Type = "M";
+            Type = "Motorcycle";
+            BrandName = GenBrand();
         }
+        public string GenBrand()
+        {
+            string Brand = " ";
+            Random rnd = new Random();
+            int brand = rnd.Next(1, 6);
+            switch (brand)
+            {
+                case 1: Brand = "Harley";
+                    break;
+                case 2:Brand = "Duccatti";
+                    break;
+                case 3:Brand = "Honda";
+                    break;
+                case 4: Brand = "Kawasaki";
+                    break;
+                case 5:
+                    Brand = "Indian";
+                    break;
+                case 6:
+                    Brand = "WestCoast Choppers";
+                    break;
+
+            }
+            return Brand;
+        }
+
 
     }
 }
